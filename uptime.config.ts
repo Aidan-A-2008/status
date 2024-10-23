c0a7a5536ff2b24707840d84213b80f124c3c05b
Aidan A <legotrain2008@gmail.com>
2024-10-23 12:07:55 -0500

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,7 +1,5 @@
 const pageConfig = {
-  // Title for your status page
   title: "Status - Aidan Arredondo",
-  // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
     { link: 'https://www.aidanarredondo.com/', label: 'Website' },
     { link: 'https://photos.aidanarredondo.com/', label: 'Photography' },
@@ -10,19 +8,14 @@ const pageConfig = {
 }
 
 const workerConfig = {
-  // Write KV at most every 3 minutes unless the status changed
   kvWriteCooldownMinutes: 3,
-  // Enable HTTP Basic auth for status page & API by uncommenting the line below, format `<USERNAME>:<PASSWORD>`
-  // passwordProtection: 'username:password',
-  // Define all your monitors here
   monitors: [
-    // Example HTTP Monitor
     {
       id: 'website',
       name: 'Website',
-      method: 'GET', // Change to GET for availability checks
+      method: 'GET',
       target: 'https://www.aidanarredondo.com',
-      expectedCodes: [200], // Expect 200 for the site to be available
+      expectedCodes: [200],
       timeout: 10000,
       headers: {
         'User-Agent': 'Uptimeflare',
@@ -31,9 +24,9 @@ const workerConfig = {
     {
       id: 'photography',
       name: 'Photography',
-      method: 'GET', // Change to GET for availability checks
+      method: 'GET',
       target: 'https://photos.aidanarredondo.com',
-      expectedCodes: [200], // Expect 200 for the site to be available
+      expectedCodes: [200],
       timeout: 10000,
       headers: {
         'User-Agent': 'Uptimeflare',
@@ -42,42 +35,14 @@ const workerConfig = {
     {
       id: 'linksite',
       name: 'Link Site',
-      method: 'GET', // Change to GET for availability checks
+      method: 'GET',
       target: 'https://aidana.link',
-      expectedCodes: [200], // Expect 200 for the site to be available
+      expectedCodes: [200],
       timeout: 10000,
       headers: {
         'User-Agent': 'Uptimeflare',
       },
     },
-
-    // Example TCP Monitor
-    //{
-     // id: 'forestracksdirectadmin',
-      //name: 'ForestRacks Direct Admin',
-      // `method` should be `TCP_PING` for tcp monitors
-      //method: 'TCP_PING',
-      // `target` should be `host:port` for tcp monitors
-      //target: '109.122.46.4',
-      //tooltip: 'My production server SSH',
-      //statusPageLink: 'https://example.com',
-      //timeout: 5000,
-    //},
-  //],
-  //notification: {
-    // [Optional] apprise API server URL
-    // if not specified, no notification will be sent
-    //appriseApiServer: "https://apprise.example.com/notify",
-    // [Optional] recipient URL for apprise, refer to https://github.com/caronc/apprise
-    // if not specified, no notification will be sent
-    //recipientUrl: "tgram://bottoken/ChatID",
-    // [Optional] timezone used in notification messages, default to "Etc/GMT"
-    //timeZone: "Asia/Shanghai",
-    // [Optional] grace period in minutes before sending a notification
-    // notification will be sent only if the monitor is down for N continuous checks after the initial failure
-    // if not specified, notification will be sent immediately
-    //gracePeriod: 5,
-  },
   ],
   callbacks: {
     onStatusChange: async (
